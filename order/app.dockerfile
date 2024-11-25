4d9751224b7f25b9cef84a1c4e559fcf9e259d56
OmShirke <113794365+OmShirke@users.noreply.github.com>
2024-11-26 04:21:47 +0530

    order microservice added

---
+++ order/app.dockerfile
@@ -0,0 +1,37 @@
+# Build Stage
+FROM golang:1.20-alpine3.20.3 AS build
+
+# Install build dependencies
+RUN apk --no-cache add gcc g++ make ca-certificates
+
+# Set the working directory inside the container
+WORKDIR /go/src/github.com/OmShirke/gRPC-Microservices
+
+# Copy dependency files
+COPY go.mod go.sum ./
+
+# Download dependencies
+RUN go mod download
+
+# Copy vendor and service-specific files
+COPY vendor vendor
+COPY order order
+
+# Build the Go application
+RUN GO111MODULE=on go build -mod vendor -o /go/bin/order-service ./order/cmd/order
+
+# Runtime Stage
+FROM alpine:3.20.3
+
+# Set the working directory
+WORKDIR /usr/bin
+
+# Copy the binary from the build stage
+COPY --from=build /go/bin/order-service .
+
+# Expose the service port
+EXPOSE 8080
+
+# Run the application
+CMD ["order-service"]
+
---
+++ order/cmd/order/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/OmShirke/gRPC-Microservices/order"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+	AcconutURL  string `envconfig:"ACCOUNT_SERVICE_URL"`
+	CatalogURL  string `envconfig:"CATALOG_SERVICE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var r order.Repo
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = order.NewPostgresRepo(cfg.DatabaseURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	})
+	defer r.Close()
+	log.Println("Listening on port 8080...")
+	s := order.NewService(r)
+	log.Fatal(order.ListenGRPC(s, cfg.AcconutURL, cfg.CatalogURL, 8080))
+}
